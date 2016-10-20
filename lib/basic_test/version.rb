db9ba1fd375bc97e4da2e6cad31b4402b2a2ebf4
Kevin Sullivan <kevinmsullivan@protonmail.com>
2016-10-19 21:50:20 -0700

    Initial commit

---
+++ lib/basic_test/version.rb
@@ -0,0 +1,3 @@
+module BasicTest
+  VERSION = "0.1.0"
+end
---
+++ spec/basic_test_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe BasicTest do
+  it 'has a version number' do
+    expect(BasicTest::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(true).to eq(true)
+  end
+end
